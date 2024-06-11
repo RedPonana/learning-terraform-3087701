4378985457f6ef640e52b09234d0eb729874fee2
RedPonana <123472569+RedPonana@users.noreply.github.com>
2024-06-11 19:45:39 +0200

    quotation marks

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ vpc_security_group_ids = [aws_security_group.blog.id]
 }
 
 resource "aws_security_group" "blog" {
-  name        = blog
+  name        = "blog"
   description = "Allow http and https in. Allow everything out."
 
   vpc_id = data.aws_vpc.default.id
